c12d9d7aac11b70e8283c455ace932d7e3f8d25b
Dominik Zaczek <dominik.brendan@gmail.com>
2021-07-04 13:02:46 +0100

    Remove package-lock

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cd92749f9f1e876eef87931c9c9d89fd'),
+      secret: env("ADMIN_JWT_SECRET", "cd92749f9f1e876eef87931c9c9d89fd"),
     },
   },
 });
