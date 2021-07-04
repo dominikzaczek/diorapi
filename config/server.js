d693b32f4c986fb3c449f317515c30d5ea6f968a
Dominik Zaczek <dominik.brendan@gmail.com>
2021-07-04 17:33:44 +0100

    dependency 5

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 8082),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "cd92749f9f1e876eef87931c9c9d89fd"),
