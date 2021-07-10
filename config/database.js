f65bc4b1c7172f3b8a5da7c6756a9af10b6937ae
Dominik Zaczek <dominik.brendan@gmail.com>
2021-07-10 11:16:59 +0100

    Connected to MariaDb

--- config/database.js
+++ config/database.js
@@ -5,10 +5,10 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',  
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '77.68.115.165'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'diorapi_'),
-        username: env('DATABASE_USERNAME', 'maria'),
+        database: env('DATABASE_NAME', 'diorapi2_'),
+        username: env('DATABASE_USERNAME', 'maria2'),
         password: env('DATABASE_PASSWORD', '1qw56_Mi'),
       },
       options: {},
